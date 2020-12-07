bbbc2190f5d7d16cfba545c4cedd95aa255fe07a
SashaEm <sashaem2002@gmail.com>
2020-12-07 22:18:43 +0200

    Working on shotgun turret

---
+++ BalloonDefender/Assets/Scripts/BulletNoAim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletNoAim : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.localPosition.normalized* 10 * speed);
+    }
+
+}
